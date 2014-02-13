6818a8f804ffb074d706ca5ca3c85ba91bcb38c5
Michael Hutchins <mlhutch@uw.edu>
2014-02-12 16:09:40 -0800

    Starting to work on WWLLN wordpress theme.

---
+++ blog/wp-content/themes/wwlln/header.php
@@ -0,0 +1,60 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>World Wide Lightning Location Network</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<script src="http://wwlln.local/js/jquery.min.js"></script>
+		<script src="http://wwlln.local/js/config.js"></script>
+		<script src="http://wwlln.local/js/skel.min.js"></script>
+		<script src="http://wwlln.local/js/skel-panels.min.js"></script>
+		<noscript>
+			<link rel="stylesheet" href="http://wwlln.local/css/skel-noscript.css" />
+			<link rel="stylesheet" href="http://wwlln.local/css/style.css" />
+			<link rel="stylesheet" href="http://wwlln.local/css/style-desktop.css" />
+		</noscript>
+		<!--[if lte IE 9]><link rel="stylesheet" href="http://wwlln.local/css/ie9.css" /><![endif]-->
+		<!--[if lte IE 8]><script src="http://wwlln.local/js/html5shiv.js"></script><![endif]-->
+        
+        <?php add_googleanalytics() ?>
+        
+	</head>
+	<body>
+
+		<!-- Header -->
+			<div id="header-wrapper">
+
+                <!-- Header -->
+                
+				<header id="header" class="container">
+					<div class="row">
+						<div class="4u">
+						
+							<!-- Logo -->
+                            <h0><a href="http://wwlln.net" id="logo">WWLLN</a></h0>
+                            
+						</div>
+                        
+                        <!-- Nav -->
+                        <div class="8u">
+                            <nav id="nav">
+                                <a href="index.html">Home</a>
+                                <a href="network.html">The Network</a>
+                                <a href="research.html">Research</a>
+                                <a href="data.html">Data</a>
+                                <a href="hosts.html">Hosts</a>
+                                <a href="contact.html">Contact</a>
+                            </nav>
+
+                        </div>
+					</div>
+                    <div class="row">
+                        <div class="12u">
+                        
+                            <h1>World Wide Lightning Location Network</h1>
+
+                        </div>
+                    </div>
+				</header>
+            </div>
